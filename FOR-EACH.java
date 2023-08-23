daf0d3169d6b89ceaace21fc91390f95dc73e21e
Joaquin05Pagano <131872083+Joaquin05Pagano@users.noreply.github.com>
2023-08-23 08:30:58 -0300

    Create FOR-EACH.java

---
+++ FOR-EACH.java
@@ -0,0 +1,7 @@
+public class ForEach {
+
+	public static void main(String[] args) {
+		int[] numeros = {1, 3, 5, 7, 9, 8, 6, 4, 3, 2, 4, 5, 6, 7, 7}; //inicializa una variable con valores dentro
+		for(int valor : numeros) System.out.println(valor); //imprime los valores recorriendo el array "numeros"
+	}
+}
