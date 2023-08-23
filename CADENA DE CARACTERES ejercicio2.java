4810cd920dbaeb82cb94b8d39eca96dcd5a114e1
Joaquin05Pagano <131872083+Joaquin05Pagano@users.noreply.github.com>
2023-08-23 09:37:45 -0300

    Create CADENA DE CARACTERES ejercicio2.java

---
+++ CADENA DE CARACTERES ejercicio2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String nombre;
+		int numvocales = 0;
+		
+		System.out.println("Ingrese su nombre: ");
+		nombre = scanner.nextLine().toLowerCase();
+		
+		for(int i = 0; i < nombre.length(); i++) {
+			char caracter = nombre.charAt(i);
+			if(caracter == 'a'|| caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u') {
+				numvocales++;
+			}
+		}
+	System.out.println("La palabra " + nombre + "tiene " + numvocales + " vocales.");
+	}
+}
